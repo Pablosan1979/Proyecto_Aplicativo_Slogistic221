55b03024cab41b456a30de11394822a0bdd5ab29
santa <santa@DESKTOP-FS8PF7P.local>
2022-07-03 14:23:37 -0500

    nuevo

--- src/aplicativo/proyecto/formas/MAQUINAS_OK.java
+++ src/aplicativo/proyecto/formas/MAQUINAS_OK.java
@@ -8,12 +8,12 @@ package aplicativo.proyecto.formas;
  *
  * @author santa
  */
-public class MAQUINAS extends javax.swing.JFrame {
+public class MAQUINAS_OK extends javax.swing.JFrame {
 
     /**
      * Creates new form MAQUINAS
      */
-    public MAQUINAS() {
+    public MAQUINAS_OK() {
         initComponents();
     }
 
@@ -150,20 +150,21 @@ public class MAQUINAS extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MAQUINAS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MAQUINAS_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MAQUINAS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MAQUINAS_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MAQUINAS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MAQUINAS_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MAQUINAS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MAQUINAS_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MAQUINAS().setVisible(true);
+                new MAQUINAS_OK().setVisible(true);
             }
         });
     }
