ee0d5db81518184529ce75974f78bd91479daf7c
santa <santa@DESKTOP-FS8PF7P.local>
2022-07-03 16:09:42 -0500

    nuevo

--- src/aplicativo/proyecto/formas/FACTURACION_CREAR_OK.java
+++ src/aplicativo/proyecto/formas/FACTURACION_CREAR_OK.java
@@ -8,12 +8,12 @@ package aplicativo.proyecto.formas;
  *
  * @author santa
  */
-public class FACTURAS_CREAR_OK extends javax.swing.JFrame {
+public class FACTURACION_CREAR_OK extends javax.swing.JFrame {
 
     /**
      * Creates new form FACTURAS
      */
-    public FACTURAS_CREAR_OK() {
+    public FACTURACION_CREAR_OK() {
         initComponents();
     }
 
@@ -325,14 +325,22 @@ public class FACTURAS_CREAR_OK extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FACTURAS_CREAR_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FACTURACION_CREAR_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FACTURAS_CREAR_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FACTURACION_CREAR_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FACTURAS_CREAR_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FACTURACION_CREAR_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FACTURAS_CREAR_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FACTURACION_CREAR_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -345,7 +353,7 @@ public class FACTURAS_CREAR_OK extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FACTURAS_CREAR_OK().setVisible(true);
+                new FACTURACION_CREAR_OK().setVisible(true);
             }
         });
     }
--- src/aplicativo/proyecto/formas/PERSONAL_OK.java
+++ src/aplicativo/proyecto/formas/PERSONAL_OK.java
@@ -8,12 +8,12 @@ package aplicativo.proyecto.formas;
  *
  * @author santa
  */
-public class PERSONAL extends javax.swing.JFrame {
+public class PERSONAL_OK extends javax.swing.JFrame {
 
     /**
      * Creates new form PERSONAL_MTTO
      */
-    public PERSONAL() {
+    public PERSONAL_OK() {
         initComponents();
     }
 
@@ -150,21 +150,23 @@ public class PERSONAL extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PERSONAL.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PERSONAL_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PERSONAL.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PERSONAL_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PERSONAL.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PERSONAL_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PERSONAL.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PERSONAL_OK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PERSONAL().setVisible(true);
+                new PERSONAL_OK().setVisible(true);
             }
         });
     }
